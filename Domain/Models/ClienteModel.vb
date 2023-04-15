a4f6564c733f1ad72f7aae76a50711b520a9d5df
romulo-manrique <romulo.j.manrique@hotmail.com>
2023-04-14 22:10:03 -0500

    Commit 00004

--- Domain/Models/ClienteModel.vb
+++ Domain/Models/ClienteModel.vb
@@ -7,6 +7,8 @@ Public Class ClienteModel
     Private _apellidoCliente As String
     Private _telefonoCliente As String
 
+    Private state As EntityState
+
     Private Repository As IClienteRepository
 
     <Required(ErrorMessage:="Campo Cédula es Obligatorio")>
@@ -19,7 +21,6 @@ Public Class ClienteModel
             _cedula = value
         End Set
     End Property
-
     <Required>
     <StringLength(50, MinimumLength:=10, ErrorMessage:="Valor mínimo de caracteres es 10 máximo es 20 de longitud")>
     Public Property NombreCliente As String
@@ -49,4 +50,56 @@ Public Class ClienteModel
             _telefonoCliente = value
         End Set
     End Property
+
+    Public Sub New()
+        Repository = New ClienteRepository()
+
+    End Sub
+
+    Public Function SaveChanges() As String
+        Dim message = Nothing
+
+        Try
+            Dim employedDataModel As New Cliente()
+
+            employedDataModel.cedula = Cedula
+            employedDataModel.nombreCliente = NombreCliente
+            employedDataModel.apellidoCliente = ApellidoCliente
+            employedDataModel.telefonoCliente = TelefonoCliente
+
+            Select Case state
+                Case EntityState.Added
+                    Repository.Add(employedDataModel)
+                    message = "Successfuly record "
+                Case EntityState.Modified
+                    Repository.Edit(employedDataModel)
+                Case EntityState.Deleted
+                    Repository.Remove(employedDataModel.cedula)
+            End Select
+
+        Catch ex As Exception
+            message = ex.Message.ToString()
+        End Try
+
+
+        Return message
+    End Function
+
+    Public Function GetClientes() As List(Of Cliente)
+        Dim ListClienteDataModel = Repository.GetAll()
+        Dim ListClienteViewModel = New List(Of Cliente)
+
+        For Each item As Cliente In ListClienteDataModel
+            ListClienteViewModel.Add(New Cliente With {
+                .cedula = item.cedula,
+                .nombreCliente = item.nombreCliente,
+                .apellidoCliente = item.apellidoCliente,
+                .telefonoCliente = item.telefonoCliente
+            })
+
+        Next
+        Return ListClienteViewModel
+    End Function
+
+
 End Class
